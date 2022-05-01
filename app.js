42e311946525a2d112fd5e59969151e523803425
Nkanyiso Ntshangase <nkanyisontshangase349@gmail.com>
2022-05-01 12:06:23 +0200

    added functionality of clicking the take note input to open the note taking form with focus function on text

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
-alert("hello world")
+class Note {
+    constructor(id, title, text) {
+      this.id = id;
+      this.title = title;
+      this.text = text;
+    }
+  }
+  class App {
+    constructor() {
+      this.notes = [];
+
+      this.$activeForm = document.querySelector(".active-form");
+      this.$inactiveForm = document.querySelector(".inactive-form");
+      this.$noteTitle = document.querySelector("#note-title");
+      this.$noteText = document.querySelector("#note-text");
+     this.addEventListeners();
+    }
+    addEventListeners(){
+        document.body.addEventListener("click", (event) => {
+            this.handleFormClick(event);
+        })
+        
+    }
+    handleFormClick(event){
+        const isActiveFormClickedOn = this.$activeForm.contains(event.target);
+        const isInActiveFormClickedOn = this.$inactiveForm.contains(event.target);
+        
+        if(isInActiveFormClickedOn) {
+            this.$inactiveForm.style.display = "none";
+            this.$activeForm.style.display = "block";
+            this.$noteText.focus();
+        }
+       
+    }
+    
+  
+    addNote(id, { title, text }) {
+      const newNote = new Note(id, title, text);
+      this.notes = [...this.notes, newNote];
+    }
+  
+    editNote(id, { title, text }) {
+      this.notes = this.notes.map((note) => {
+        if (note.id == id) {
+          note.title = title;
+          note.text = text;
+        }
+        return note;
+      });
+    }
+  
+    deleteNote(id) {
+      this.notes = this.notes.filter((note) => note.id != id);
+    }
+  
+    displayNotes() {
+      this.notes.map((note) =>
+        console.log(`
+        ID: ${note.id}
+        Title: ${note.title}
+        Text: ${note.text}
+      `)
+      );
+    }
+  }
+  
+  const app = new App();
+  
